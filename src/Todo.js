af52e7f9033e4fa55e86ff258ab34e53d774b85f
papillon129245 <62538060+papillon129245@users.noreply.github.com>
2020-06-03 01:47:58 +0100

    first blood storybook

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Todo.css';
+
+function Todo() {
+    return (
+        <div>
+            <h1>I am a Todo</h1>
+        </div>
+    )
+}
+
+export default Todo
+
+
