e75de2c7353ac677ea4d7635a8067ad4f4b1a2ae
papillon129245 <62538060+papillon129245@users.noreply.github.com>
2020-06-04 01:02:29 +0100

    todo omponents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Todo from './Todo';
+import TodoContainer from './TodoContainer';
 
 function App() {
+  
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    setTodos([...todos,input]);
+    setInput('');
+  };
+
   return (
     <div>
       <h1>Hello Mariusz</h1>
-      <Todo />
+      <form>
+        <input value={input} 
+        onChange={e => setInput(e.target.value)} type='text' />
+        <button type='submit' 
+        onClick={addTodo}>Add Todo</button>
+
+      </form>
+      <TodoContainer todos={todos} />;
     </div>
   );
 }
--- src/TodoContainer.js
+++ src/TodoContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './TodoContainer.css';
 import Todo from './Todo';
+import { v4 as uuidv4 } from 'uuid';
 
 function TodoContainer({todos}) {
     return (
         <div className='todoContainer'>
             {todos.map(todo => (
-                <Todo title={todo} />
+                <Todo title={todo} key={uuidv4()} />
             ))}
         </div>
     )
